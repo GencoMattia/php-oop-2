69d07bac71d5394efab840844a9ccf931829bfc9
GencoMattia <gencomattia@hotmail.it>
2024-07-03 16:20:42 +0200

    Created Accessory Class

--- classes/Accessory.php
+++ classes/Accessory.php
@@ -1,5 +1,52 @@
 <?php 
 
+class Accessory extends Product {
+    private $material;
+    private $size;
 
+    public function __construct($name, $price, $imgUrl, $category, $material, $size){
+        parent::__construct($name, $price, $imgUrl, $category);
+        $this -> material = $material;
+        $this -> size = $size;
+    }
+
+    /**
+     * return Product class instance $material
+     *
+     * @return void
+     */
+    public function getMaterial(){
+        return $this -> material;
+    }
+
+    /**
+     * set $material Product class instance as a string
+     *
+     * @param [string] $name
+     * @return void
+     */
+    public function setMaterial($material){
+        return $this -> material = $material;
+    }
+
+    /**
+     * return Product class instance $size
+     *
+     * @return void
+     */
+    public function getSize(){
+        return $this -> size;
+    }
+
+    /**
+     * set $weight Product class instance as a string
+     *
+     * @param [string] $size
+     * @return void
+     */
+    public function setSize($size){
+        return $this -> size = $size;
+    }
+}
 
 ?>
