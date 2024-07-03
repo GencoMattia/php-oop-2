6e5c3fea64efa9191bec8e0ee42cc284bbfd0686
GencoMattia <gencomattia@hotmail.it>
2024-07-03 16:38:11 +0200

    Created first class obj

--- index.php
+++ index.php
@@ -6,4 +6,22 @@ require_once __DIR__ . "/classes/Plaything.php";
 require_once __DIR__ . "/classes/Accessory.php";
 require_once __DIR__ . "/classes/Category.php";
 
+$article1 = new Food ("Royal Canin Mini Adult","43,99", "https://arcaplanet.vtexassets.com/arquivos/ids/300306/Royal-Canin-Mini-Adult-Alimento-Completo-per-Cani-Adulti-di-Piccola-Taglia-10120604-1.jpg", new Category("Cane"), "prosciutto, Riso", 545);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boolshop</title>
+</head>
+<body>
+    <main>
+        <section>
+            
+        </section>
+    </main>
+</body>
+</html>
