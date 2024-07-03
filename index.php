ba50f28845fdaadc01d492de1e7491cc67e35e02
GencoMattia <gencomattia@hotmail.it>
2024-07-03 16:26:08 +0200

    required_once classe into index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
 
-
+require_once __DIR__ . "/classes/Product.php";
+require_once __DIR__ . "/classes/Food.php";
+require_once __DIR__ . "/classes/Plaything.php";
+require_once __DIR__ . "/classes/Accessory.php";
+require_once __DIR__ . "/classes/Category.php";
 
 ?>
